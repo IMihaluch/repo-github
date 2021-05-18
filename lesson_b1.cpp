88eef97926e8359a571da90bf778b212e06dc2c4
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2021-05-18 15:59:27 +0300

    Update lesson_b

--- lesson_b1.cpp
+++ lesson_b1.cpp
@@ -1,1 +1,15 @@
-some content
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+int main(int argc, const char** argv) { 
+    char letter;
+    letter = 'X';
+    bool b = true;
+    printf("%d", letter);
+   
+    enum xo {x, o};
+    using xo = enum xo;
+    xo game =x;
+    
+    std::cout << 
