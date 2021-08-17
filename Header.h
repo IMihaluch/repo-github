68d95a701dc6812db5fd29e01859b043e78cd2e5
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2021-08-17 21:22:56 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <stdarg.h>
+#include <stddef.h>
